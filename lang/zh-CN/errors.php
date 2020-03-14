6dea6d29c1e29dc55dc91188b01d596b4302a56e
陈恺垣 <chenkaiyuan1993@gmail.com>
2020-03-15 00:50:03 +0800

    完善翻译

--- lang/zh-CN/errors.php
+++ lang/zh-CN/errors.php
@@ -7,6 +7,6 @@ return [
     'message_type_error'              => 'Notification 返回的 message 类型必须是 '.Message::class,
     'aliyun_sdk_client_exception'     => '阿里云 SDK 抛出客户端异常。错误码：:code；错误内容: :message',
     'aliyun_sdk_server_exception'     => '阿里云 SDK 抛出服务端异常。错误码：:code；错误内容: :message',
-    'aliyun_api_failed'               => 'API 请求失败。。错误码：:code；错误内容: :message',
+    'aliyun_api_failed'               => 'API 请求失败。错误码：:code；错误内容: :message',
     'argument_sms_users_type_invalid' => '入参 $smsUsers 只能是数组或 '.Collection::class,
 ];
