b63bac83eed4af4a4d76e989cc8ace31e7bea73a
陈恺垣 <chenkaiyuan1993@gmail.com>
2020-03-17 00:02:12 +0800

    完善配置项与异常处理

--- config/aliyun_sms.php
+++ config/aliyun_sms.php
@@ -4,5 +4,6 @@ return [
     'access_key_id'          => env('ALIYUN_ACCESS_KEY_ID'),
     'access_key_secret'      => env('ALIYUN_ACCESS_KEY_SECRET'),
     'region'                 => env('ALIYUN_SMS_REGION'),
+    'host'                   => env('ALIYUN_SMS_HOST', 'dysmsapi.aliyuncs.com'),
     'access_key_client_name' => env('ALIYUN_SMS_ACCESS_KEY_CLIENT_NAME', 'sms'),
 ];
--- src/AliyunSmsChannel.php
+++ src/AliyunSmsChannel.php
@@ -39,9 +39,12 @@ class AliyunSmsChannel
             $accessKeyId = config('aliyun_sms.access_key_id');
             $accessKeySecret = config('aliyun_sms.access_key_secret');
             $region = config('aliyun_sms.region');
+            $host = config('aliyun_sms.host');
             $clientName = config('aliyun_sms.access_key_client_name');
 
-            AlibabaCloud::accessKeyClient($accessKeyId, $accessKeySecret)->regionId($region)->name($clientName);
+            AlibabaCloud::accessKeyClient($accessKeyId, $accessKeySecret)
+                ->regionId($region)
+                ->name($clientName);
 
             $options = $message->getMessage();
             $options['RegionId'] = $region;
@@ -52,20 +55,19 @@ class AliyunSmsChannel
                 ->client($clientName)
                 ->scheme('https')
                 ->action($action)
-                ->host('dysmsapi.aliyuncs.com')
+                ->host($host)
                 ->options([
                     'query' => $options,
                 ])
                 ->request();
 
-            $result = $response->toArray();
             // Code 为 Ok 时表示发送成功
             // 文档 https://help.aliyun.com/document_detail/101346.html?spm=a2c4g.11186623.6.621.cbd42246K5pp8V
-            if ($result['Code'] != 'OK') {
+            if ($response->get('Code') != 'OK') {
                 throw new Exception(
                     __('aliyun_sms::errors.aliyun_api_failed', [
-                        'code'    => $result['Code'],
-                        'message' => $result['Message'],
+                        'code'    => $response->get('Code'),
+                        'message' => $response->get('Message'),
                     ]),
                     ErrorCodes::ALIYUN_API_FAILED
                 );
